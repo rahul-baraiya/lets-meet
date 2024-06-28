59c12c4854096b2ac0af85b9ac27b4081d757283
Rahul Baraiya <107394172+rahul-baraiya@users.noreply.github.com>
2024-06-28 16:12:55 +0530

    Update and rename index.html to app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    console.log(`User joined room: ${roomId}`);
+    socket.to(roomId).emit('userJoined', roomId);
+  });
+
+  socket.on('leave', (roomId) => {
+    socket.leave(roomId);
+    io.to(roomId).emit('userLeft');
+    console.log(`User left room: ${roomId}`);
+  });
+
+  socket.on('offer', (data) => {
+    io.to(data.roomId).emit('offer', data.sdp);
+  });
+
+  socket.on('answer', (data) => {
+    io.to(data.roomId).emit('answer', data.sdp);
+  });
+
+  socket.on('candidate', (data) => {
+    io.to(data.roomId).emit('candidate', data.candidate);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
